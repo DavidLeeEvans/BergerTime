6243eae1e0520399d6b243465dd9291f7661c4df
David Lee Evans <dle.evans@gmail.com>
2023-01-21 18:13:58 -0500

    wip

--- CandyBerger/src/Main.hx
+++ CandyBerger/src/Main.hx
@@ -1,64 +1,68 @@
-import defold.Go;
-import defold.Msg;
-import defold.Particlefx;
-import defold.Collectionproxy;
+/**
+ * File: Main.hx
+ * For: Main Boot Screen
+ * By: David Lee Evans
+ * Created:
+ * Modified:
+ *
+ */
 
+import defold.Collectionproxy.CollectionproxyMessages;
+import defold.Go.GoMessages;
+import defold.Msg;
+import defold.Sys.SysSysInfo;
 import defold.types.Message;
 import defold.types.Url;
+import hud.LanguageMessage;
+import lua.Table.AnyTable;
 
 typedef MainData = {
-	var state:MainState;
-}
-
-@:enum abstract MainState(String) {
-	var MAIN_MENU = "MAIN_MENU";
-	var EXIT_MENU = "EXIT_MENU";
-	var SETTINGS_MENU = "SETTINGS_MENU";
-	var GAME_RUNNING = "GAME_RUNNING";
+	var next_level:String;
+	var last_level:String;
+	// Settings And Configurations
+	var lang:String;
+	var data_beam:lua.AnyTable;
 }
 
 class Main extends defold.support.Script<MainData> {
-	override function init(self:MainData) {
-		Msg.post("#", Messages.to_main_menu);
-		self.state = MAIN_MENU;
-		Particlefx.play("#particlefx");
+	override function init(self:MainData):Void {
+		Msg.post(".", GoMessages.acquire_input_focus);
+		// Begining of System Inits
+		final sysSysInfo:SysSysInfo = defold.Sys.get_sys_info();
+		if (sysSysInfo.system_name == "Linux")
+		//	DefOS.set_always_on_top(true);
+		self.lang = sysSysInfo.device_language;
+		self.data_beam = SaveLoad.get_all_saved_data(); // TODO Some Y2k like testing needed!!!
+		self.lang = self.data_beam.lang;
+		Defold.pprint(self.data_beam); // Recycle data_beam to advoid race lock condition Facebook.
+		// End of System Inits
+		self.next_level = "/go#splash_screen";
+		Msg.post(self.next_level, CollectionproxyMessages.load);
 	}
 
-	override function on_message<T>(self:MainData, message_id:Message<T>, message:T, sender:Url) {
+	override function on_message<TMessage>(self:MainData, message_id:Message<TMessage>, message:TMessage, sender:Url):Void {
 		switch (message_id) {
-			case Messages.to_main_menu:
-				trace("Main Menu");
-				if (self.state != MAIN_MENU)
-					Msg.post("#boardproxy", CollectionproxyMessages.unload);
-				Msg.post("main:/main#menu", GoMessages.enable);
-				Msg.post("#background", GoMessages.enable);
-				self.state = MAIN_MENU;
-
-			case Messages.start_game:
-				trace(" Start Game Message Received ");
-				Msg.post("#background", GoMessages.disable);
-				// FIXME Examine DLE Msg.post("#boardproxy", CollectionproxyMessages.load);
-				Msg.post("#menu", GoMessages.disable);
-			case Messages.exit_game:
-				trace(" Exit Message Received ");
-				self.state = EXIT_MENU;
-			// TODO Post Message Exit Game
-			case Messages.settings:
-				trace(" Settings Message Received ");
-				Msg.post("#background", GoMessages.disable);
-				Msg.post("#menu", GoMessages.disable);
-				self.state = SETTINGS_MENU;
-			// TODO Post Message Settings
 			case CollectionproxyMessages.proxy_loaded:
-				// Board collection has loaded...
-				Msg.post(sender, CollectionproxyMessages.init);
-				Msg.post("board:/board#script", Messages.start_level, {difficulty: 1});
-				Msg.post(sender, GoMessages.enable);
-				self.state = GAME_RUNNING;
-			case Messages.pause_off:
-				trace('Game is Paused');
-			case Messages.pause_on:
-				trace('Game is UnPaused');
+				Msg.post(self.next_level, CollectionproxyMessages.enable);
+				if (self.last_level != null) {
+					Msg.post(self.last_level, CollectionproxyMessages.unload);
+				}
+			case MainMessages.remote_new_screen:
+				self.last_level = message.current;
+				self.next_level = message.next;
+				Msg.post(self.next_level, CollectionproxyMessages.load);
+			case LanguageMessage.set_lang:
+				trace('Received lang ${message.lang}');
+				self.lang = message.lang;
+				self.data_beam.lang = self.lang;
+				SaveLoad.save_all_data(self.data_beam);
+
+			case LanguageMessage.get_lang:
+				Msg.post(message.id, LanguageMessage.have_lang, {lang: self.lang});
 		}
+	}
+
+	override function final_(_):Void {
+		Msg.post(".", GoMessages.release_input_focus);
 	}
 }
--- CandyBerger/src/Tools.hx
+++ CandyBerger/src/Tools.hx
@@ -1,13 +1,29 @@
-import defold.types.Vector4;
-import defold.types.Vector3;
-
 import defold.Msg;
-
 import defold.Render.RenderMessages;
+import defold.types.Vector3;
+import defold.types.Vector4;
 
-// Some more Assembly needed.
 class Tools {
 	public static function draw_line(from:Vector3, to:Vector3, ?color:Vector4):Void {
 		Msg.post("@render:", RenderMessages.draw_line, {start_point: from, end_point: to, color: defold.Vmath.vector4(1, 0, 0, 1)});
+	}
+
+	public static function level_int_to_string(level:Int, ?prefix:String):String {
+		var r:String = "";
+		if (prefix == null) {
+			prefix = "";
+		}
+		if (level >= 0 && level <= 9) {
+			r = prefix + '0' + Std.string(level);
+		} else {
+			r = prefix + Std.string(level);
+		}
+		return r;
+	}
+
+	public static function translate_text(lang:String, text:String):String {
+		var r:String = text;
+
+		return r;
 	}
 }
