f2aad318fe1bb9cfcc73835a8ee7318ac8faedc0
David Lee Evans <dle.evans@gmail.com>
2023-01-20 19:45:27 -0500

    wip

--- bergertime/game_levels/loader.go
+++ bergertime/game_levels/loader.go
@@ -216,21 +216,3 @@ embedded_components {
     w: 1.0
   }
 }
-embedded_components {
-  id: "splashroom_proxy"
-  type: "collectionproxy"
-  data: "collection: \"/splash/splashscreenrive.collection\"\n"
-  "exclude: false\n"
-  ""
-  position {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-  }
-  rotation {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0
-  }
-}
