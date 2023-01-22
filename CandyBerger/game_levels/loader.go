5edafddab2a96b8dbb2d0340ff8eb65bf6010970
David Lee Evans <dle.evans@gmail.com>
2023-01-22 14:34:51 -0500

    wip

--- CandyBerger/game_levels/loader.go
+++ CandyBerger/game_levels/loader.go
@@ -139,24 +139,7 @@ embedded_components {
     w: 1.0
   }
 }
-embedded_components {
-  id: "menu_proxy"
-  type: "collectionproxy"
-  data: "collection: \"/menu/Menu.collection\"\n"
-  "exclude: false\n"
-  ""
-  position {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-  }
-  rotation {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0
-  }
-}
+
 embedded_components {
   id: "settings_proxy"
   type: "collectionproxy"
