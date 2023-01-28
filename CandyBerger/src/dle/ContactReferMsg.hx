fcf12089a119a40d01ffa068c37f84395340d6f2
David Lee Evans <dle.evans@gmail.com>
2023-01-27 21:44:53 -0500

    wip

--- CandyBerger/src/dle/ContactReferMsg.hx
+++ CandyBerger/src/dle/ContactReferMsg.hx
@@ -3,9 +3,9 @@ package dle;
 @:build(defold.support.MessageBuilder.build())
 class ContactReferMsg {
 	var cancel_pane;
-	var set_url:{url_loc:String};
-	var set_html:{html:String};
-	var set_title:{title:String};
-	var set_body:{boy:String};
+	var set_url:String;
+	var set_html:String;
+	var set_title:String;
+	var set_body:String;
 	var show_pane;
 }
