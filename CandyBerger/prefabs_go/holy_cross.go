13905054ba472f4a74c3e3875d8c2d2ce7f3dff9
David Lee Evans <dle.evans@gmail.com>
2023-02-05 13:31:10 -0500

    wip

--- CandyBerger/prefabs_go/holy_cross.go
+++ CandyBerger/prefabs_go/holy_cross.go
@@ -13,6 +13,21 @@ components {
     w: 1.0
   }
 }
+components {
+  id: "fixture"
+  component: "/entities_go/floors/fixtures.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
 embedded_components {
   id: "collisionobject"
   type: "collisionobject"
